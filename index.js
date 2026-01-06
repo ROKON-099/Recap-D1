062394e7af53ecf3d3a6e6f4441221f0e808fe81
Rokon007 <rokon@example.com>
2026-01-06 21:49:09 +0600

    added a commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let h1=document.getElementById('title-1');
+let h2=document.getElementById('title-2');
+let h2again=document.getElementById('title-3');
+
+
+// change in text ;
+h1.textContent="My name is rokon";
+h2.textContent="My name is rokon";
+h2again.textContent="My name is rokon";
+  // change in colour 
+  h1.style.color="red";
+   // select by class name;
+   let class1 =document.querySelector(".name");
+   
