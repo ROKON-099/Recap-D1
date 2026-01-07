11b9aed9727455e6bdf2cf2a649cb175f04f2f1e
Rokon007 <rokon@example.com>
2026-01-07 20:53:51 +0600

    added a commit

---
+++ card/index.js
@@ -0,0 +1,17 @@
+function sayhello (){
+    console.log("hello world");
+
+}
+sayhello();
+// arrow function;
+const add =(a,b) => {
+return a+b;
+
+};
+const add =(a,b) => 
+{
+    return a+b;
+
+}
+// javascript practice 
+
